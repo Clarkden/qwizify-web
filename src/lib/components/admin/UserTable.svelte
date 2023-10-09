e972470a9a3230e8ec107df5420745d4c754b75b
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-10-09 12:54:30 -0700

    Added admin dashboard

--- src/lib/components/admin/UserTable.svelte
+++ src/lib/components/admin/UserTable.svelte
@@ -36,7 +36,7 @@
 </script>
 
 <div class="flex flex-col w-full gap-3">
-  <h1 class="text-3xl font-bold">Users</h1>
+  <h1 class="text-xl font-bold">Users</h1>
   {#if users && users.length > 0}
     <div class="flex flex-col gap-3 w-full">
       {#each users as user}
