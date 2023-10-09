a0d4f8706376d0460704517fb9f3b9eb681cb3e5
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-10-09 12:57:06 -0700

    Bug Fix

--- src/lib/components/admin/UserTable.svelte
+++ src/lib/components/admin/UserTable.svelte
@@ -1,38 +1,9 @@
 <script lang="ts">
-  import UserRow from "./userRow.svelte";
-  import { createTable, Render, Subscribe } from "svelte-headless-table";
-  import { readable } from "svelte/store";
+  import UserRow from "$lib/components/admin/UserRow.svelte";
+
   import * as Table from "$lib/components/ui/table";
 
   export let users: any;
-
-  const table = createTable(readable(users));
-
-  const columns = table.createColumns([
-    table.column({
-      accessor: "id",
-      header: "ID",
-    }),
-    // table.column({
-    //   accessor: "status",
-    //   header: "Status"
-    // }),
-    table.column({
-      accessor: "email",
-      header: "Email",
-    }),
-    table.column({
-      accessor: "role",
-      header: "Role",
-    }),
-    table.column({
-      accessor: ({ email }) => email,
-      header: "",
-    }),
-  ]);
-
-  const { headerRows, pageRows, tableAttrs, tableBodyAttrs } =
-    table.createViewModel(columns);
 </script>
 
 <div class="flex flex-col w-full gap-3">
@@ -45,40 +16,3 @@
     </div>
   {/if}
 </div>
-
-<!-- <div class="rounded-md border">
-  {#if users && users.length > 0}
-    <Table.Root {...$tableAttrs}>
-      <Table.Header>
-        {#each $headerRows as headerRow}
-          <Subscribe rowAttrs={headerRow.attrs()}>
-            <Table.Row>
-              {#each headerRow.cells as cell (cell.id)}
-                <Subscribe attrs={cell.attrs()} let:attrs props={cell.props()}>
-                  <Table.Head {...attrs}>
-                    <Render of={cell.render()} />
-                  </Table.Head>
-                </Subscribe>
-              {/each}
-            </Table.Row>
-          </Subscribe>
-        {/each}
-      </Table.Header>
-      <Table.Body {...$tableBodyAttrs}>
-        {#each $pageRows as row (row.id)}
-          <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-            <Table.Row {...rowAttrs}>
-              {#each row.cells as cell (cell.id)}
-                <Subscribe attrs={cell.attrs()} let:attrs>
-                  <Table.Cell {...attrs}>
-                    <Render of={cell.render()} />
-                  </Table.Cell>
-                </Subscribe>
-              {/each}
-            </Table.Row>
-          </Subscribe>
-        {/each}
-      </Table.Body>
-    </Table.Root>
-  {/if}
-</div> -->
