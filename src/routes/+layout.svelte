28b65cea753ad02c2e7cd5f07d7d85402f5e0445
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-10-08 20:27:12 -0700

    Try asgain

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
   import "../app.css";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <body class="bg-background text-white flex flex-col min-h-screen">
