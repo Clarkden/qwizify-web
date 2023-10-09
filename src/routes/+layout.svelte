9c4a3941d59fcb1d6cd41863611df7d0b569a14c
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-10-08 20:28:13 -0700

    Fixed i think

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script>
   import "../app.css";
-  import { dev } from "$app/environment";
-  import { inject } from "@vercel/analytics";
 
-  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <body class="bg-background text-white flex flex-col min-h-screen">
