6e812d3e06ef98ef94e55f02a542b6bc8bf9894e
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-10-09 13:12:25 -0700

    Admin Page Changes

--- src/lib/components/admin/UserRow.svelte
+++ src/lib/components/admin/UserRow.svelte
@@ -2,7 +2,11 @@
   export let user: any;
 </script>
 
-<div class="border border-secondary rounded-md p-5 bg-background w-full">
-  <h2 class="font-bold">{user.email}</h2>
-  <p class="text-muted-primary">{user.role}</p>
+<div class=" py-2 w-full grid grid-cols-2 px-2 ">
+  <div class="col-span-1">
+    <h2>{user.email}</h2>
+  </div>
+  <div class="col-span-1">
+    <p class="text-green-400 text-right">{user.role}</p>
+  </div>
 </div>
--- src/lib/components/admin/UserTable.svelte
+++ src/lib/components/admin/UserTable.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
   import UserRow from "$lib/components/admin/UserRow.svelte";
-
   import * as Table from "$lib/components/ui/table";
+  import { Input } from "$lib/components/ui/input";
+  import Button from "../ui/button/button.svelte";
 
   export let users: any;
 </script>
 
 <div class="flex flex-col w-full gap-3">
-  <h1 class="text-xl font-bold">Users</h1>
+  <div class="flex flex-row gap-2 items-center">
+    <h1 class="text-xl font-bold">Users</h1>
+  </div>
+  <div class="flex flex-row gap-2 items-center">
+    <Button variant="secondary">Search</Button>
+    <Input placeholder="Search" />
+  </div>
   {#if users && users.length > 0}
-    <div class="flex flex-col gap-3 w-full">
+    <div
+      class="flex flex-col gap-3 w-full divide-y divide-secondary border p-2 rounded border-secondary"
+    >
       {#each users as user}
         <UserRow {user} />
       {/each}
