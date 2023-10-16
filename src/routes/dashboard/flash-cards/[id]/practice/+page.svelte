f5422419213c304a7f745b01767e29077d195b3d
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-10-16 14:24:00 -0700

    Something is wrong with my deployment

--- src/routes/dashboard/flash-cards/[id]/practice/+page.svelte
+++ src/routes/dashboard/flash-cards/[id]/practice/+page.svelte
@@ -16,10 +16,20 @@
 <div
   class="col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-6 overflow-y-auto overflow-x-hidden"
 >
-  <section class="p-5 sm:mx-auto w-full sm:w-2/3 md:w-3/5">
+  <section class="p-5 sm:mx-auto w-full sm:w-2/3 md:w-3/5 hidden md:block">
     <Button
       variant="ghost"
       on:click={() => goto("/dashboard/flash-cards/" + $page.params.id)}
+      class="flex flex-row gap-2 items-center"
+      ><ArrowLeft class="w-5 h-5" /> Back</Button
+    >
+  </section>
+  <section
+    class="px-2 w-full flex flex-row md:hidden items-center justify-between mt-12"
+  >
+    <Button
+      variant="ghost"
+      on:click={() => goto("/dashboard/" + $page.params.id)}
       class="flex flex-row gap-2 items-center"
       ><ArrowLeft class="w-5 h-5" /> Back</Button
     >
